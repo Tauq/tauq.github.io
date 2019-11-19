ab7f742881a0e1f2c594b08bd9f786a670f70929
Tauq <55885195+Tauq@users.noreply.github.com>
2019-11-19 11:50:57 +0000

    Fixing menu display cut-off problem due to last change

--- PhotographyPortfolio/Assets/js/global.js
+++ PhotographyPortfolio/Assets/js/global.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 			$(".menuAtTopNav").css("display","block");      // This makes the menu appear
 			$(".homePage__hero").css("position","static");  // Setting this to static rather than relative makes the menu appear correctly, otherwise it gets cut off at the bottom
 			$(".homePage__text").css("display","none");     // This is a work-around - make text over image disappear when menu is visible - avoids problem with words going further down the page when position is changed to static
+			$(".sectionTitle").css("position","static");    // Setting this to static rather than relative makes the menu appear correctly, otherwise it gets cut off at the bottom
 
 	});
 
@@ -13,6 +14,7 @@ $(document).ready(function(){
 			$(".menuAtTopNav").css("display","none");        // This makes the menu disappear
 			$(".homePage__hero").css("position","relative"); // Put this setting back to as it was before the menu was displayed, so the text is displayed over the image again
 			$(".homePage__text").css("display","block");     // Put the text back to as it was before the menu was displayed
+			$(".sectionTitle").css("position","relative");  // Setting this to static rather than relative makes the menu appear correctly, otherwise it gets cut off at the bottom
 	});
 
 });
