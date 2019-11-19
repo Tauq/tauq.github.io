35f93208eb095b29b6d6ed86975b76246ddb64f0
Tauq <55885195+Tauq@users.noreply.github.com>
2019-11-19 11:23:37 +0000

    Added functiionaliy to search magnifying glass on homepage

--- PhotographyPortfolio/Assets/js/homePage.js
+++ PhotographyPortfolio/Assets/js/homePage.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
 
+	$(".searchIcon").click(function(){
+		event.preventDefault();
+			$(".homePage__searchInput").toggle();      // This makes the search input box toggle between appearing and disappearing
+	});
 
 });
